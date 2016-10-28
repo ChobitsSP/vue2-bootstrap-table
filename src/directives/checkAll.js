1c5bb4a05774889eccde9c7b112579d502780949
Chobits <chobits@live.cn>
2016-10-28 14:01:27 +0800

    debug

--- src/directives/checkAll.js
+++ src/directives/checkAll.js
@@ -1,18 +1,4 @@
 ﻿module.exports = {
-    bind: function (el, binding, vnode) {
-        vnode.handler = function (e) {
-            let val = e.target.checked
-
-            //var arr = []
-            //if (el.checked) {
-            //    //this.params.array.forEach(function (item) {
-            //    //    arr.push(item)
-            //    //})
-            //}
-        }
-
-        el.addEventListener('change', vnode.handler)
-    },
     update: function (el, binding, vnode, oldVnode) {
         let len1 = binding.value[0]
         let len2 = binding.value[1]
@@ -20,8 +6,5 @@
 
         el.checked = checked
         el.indeterminate = !checked && len1 > 0
-    },
-    unbind: function (el, binding, vnode) {
-        el.removeEventListener('click', vnode.handler)
     }
 }
