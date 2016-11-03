e2ba9f4a760b5850a6b1605879af67beda5f6a12
Chobits <chobits@live.cn>
2016-11-03 13:33:21 +0800

    ShowColumns

---
+++ src/components/ShowColumns.vue
@@ -0,0 +1,46 @@
+﻿<template>
+    <div class="keep-open btn-group" :class="{ open: show }" ref="root">
+        <button type="button" class="btn btn-default dropdown-toggle" @click="show=!show">
+            <i class="glyphicon glyphicon-th icon-th">
+            </i>
+            <span class="caret">
+            </span>
+        </button>
+        <ul class="dropdown-menu" v-show="show" aria-labelledby="simple-dropdown">
+            <li v-for="(col, index) in columns">
+                <label>
+                    <input type="checkbox" v-model="col.visible">
+                    {{ col.title }}
+                </label>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import $ from 'src/utils/NodeList.js'
+
+    module.exports = {
+        props: {
+            columns: {
+                type: Array
+            }
+        },
+        data() {
+            return {
+                show: false
+            }
+        },
+        mounted() {
+            let self = this
+
+            this.$nextTick(function () {
+                const $el = $(this.$refs.root)
+
+                $el.onBlur((e) => {
+                    self.show = false
+                })
+            })
+        }
+    }    
+</script>
