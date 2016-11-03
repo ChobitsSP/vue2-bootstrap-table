4fcd728ab6bbbc8049a0fe93ec417169a847fbd2
Chobits <chobits@live.cn>
2016-11-03 15:14:40 +0800

    add demo

---
+++ src/dev/DemoClient.vue
@@ -0,0 +1,161 @@
+﻿<template>
+    <div class="bootstrap-table">
+        <div class="fixed-table-toolbar">
+            <div class="bs-bars pull-left">
+                <div>
+                    <button id="remove" class="btn btn-danger" @click="remove_all">
+                        <i class="glyphicon glyphicon-remove"></i> Delete
+                    </button>
+                </div>
+            </div>
+            <div class="columns columns-right btn-group pull-right">
+                <button class="btn btn-default" type="button" name="refresh" title="Refresh" @click="refresh">
+                    <i class="glyphicon glyphicon-refresh icon-refresh"></i>
+                </button>
+                <show-columns :columns="columns"></show-columns>
+            </div>
+            <div class="pull-right search">
+                <input class="form-control" type="text" placeholder="Search" v-model="q">
+            </div>
+        </div>
+        <div class="fixed-table-container">
+            <div class="fixed-table-body">
+                <table-client :columns="columns"
+                              :rows="rows"
+                              :total="total_result"
+                              :loading="loading"
+                              :init-pager="pager"
+                              :config="config"
+                              :q="q"
+                              @init-hub="init_hub">
+                </table-client>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    let Vue = require('vue')
+    let ajax = require('./ajax.js')
+
+    module.exports = {
+        data() {
+            return {
+                q: null,
+                columns: [],
+                rows: [],
+                checklist: [],
+                total_result: 0,
+                loading: false,
+                config: {
+                    checkbox: true
+                },
+                pager: {
+                    page_no: 1,
+                    page_size: 10,
+                    sort_name: 'id',
+                    is_desc: false
+                }
+            }
+        },
+        created() {
+
+            ['id', 'name', 'nickname', 'email', 'birthdate'].forEach(t => {
+                this.columns.push({
+                    title: t,
+                    field: t,
+                    visible: true,
+                    sortable: true
+                })
+            })
+
+            this.columns.push({
+                title: 'gender',
+                field: 'gender',
+                filter(val) {
+                    return val === 'M' ? '♂' : '♀'
+                },
+                visible: true,
+                sortable: true
+            })
+
+            this.columns.push({
+                title: 'created date',
+                field: 'created_at',
+                filter(val) {
+                    return val.substr(0, 10)
+                },
+                visible: true,
+                sortable: true
+            })
+
+            let c1 = require('./Btns.vue')
+
+            let btn_hub = this.btn_hub = new Vue()
+
+            this.columns.push({
+                title: 'edit/remove',
+                sortable: false,
+                visible: true,
+                createElement: function (createElement, row, col, index) {
+                    var com1 = createElement(c1, {
+                        props: {
+                            row: row,
+                            eventHub: btn_hub,
+                            index: index
+                        }
+                    })
+
+                    return createElement('td', {}, [com1])
+                }
+            })
+
+            this.btn_hub.$on('edit', this.edit)
+            this.btn_hub.$on('remove', this.remove)
+            this.refresh()
+        },
+        methods: {
+            init_hub(hub) {
+                this.tb_hub = hub
+                this.tb_hub.$on('refresh', this.child_refresh)
+                this.tb_hub.$on('checklist', this.sync_checklist)
+            },
+            sync_checklist(list) {
+                this.checklist = list
+            },
+            child_refresh(pager) {
+                this.pager.page_no = pager.page_no
+                this.pager.page_size = pager.page_size
+                this.pager.sort_name = pager.sort_name
+                this.pager.is_desc = pager.is_desc
+                this.refresh()
+            },
+            refresh() {
+                this.rows = require('!json!./data.json')
+            },
+            edit(row, index) {
+                this.rows[index].name = new Date().getTime().toString()
+            },
+            remove(row, index) {
+                this.rows.splice(index, 1)
+            },
+            remove_all() {
+                //this.checklist.forEach(function (t) {
+                //    this.remove(t)
+                //}.bind(this))
+                this.tb_hub.$emit('clear-checklist')
+            }
+        },
+        components: {
+            ShowColumns: require('components/ShowColumns.vue'),
+            TableClient: require('src/TableClient.vue')
+        },
+        beforeDestroy: function () {
+            this.tb_hub.$off('refresh', this.child_refresh)
+            this.tb_hub.$off('checklist', this.sync_checklist)
+
+            this.btn_hub.$off('edit', this.edit)
+            this.btn_hub.$off('remove', this.remove)
+        }
+    }
+</script>
---
+++ src/utils/pager.js
@@ -0,0 +1,13 @@
+﻿// Load the full build.
+var _ = require('lodash')
+
+function getPaginatedItems(items, page, per_page) {
+    let offset = (page - 1) * per_page
+    return _.chain(items).slice(offset).take(per_page).value()
+}
+
+module.exports = function (data, pager) {
+    let items = _.orderBy(data, pager.sort_name, pager.is_desc ? 'desc' : 'asc')
+    items = getPaginatedItems(items, pager.page_no, pager.page_size)
+    return items
+}
