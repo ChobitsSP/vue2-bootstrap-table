07875c51b0538ce4f319ac4fb5261c417cda15f4
Chobits <chobits@live.cn>
2016-11-07 10:50:57 +0800

    add router

--- src/dev.js
+++ src/dev.js
@@ -1,12 +1,16 @@
 ﻿import Vue from 'vue'
 import VueResource from 'vue-resource'
 import App from './dev/App.vue'
+import router from './dev/router.js'
 
 window.Vue = Vue
 
 Vue.use(VueResource)
 
-new Vue({
-    el: '#app',
-    ...App
-})
+const app = new Vue({
+    template: '<app />',
+    router: router,
+    components: {
+        App: App
+    }
+}).$mount('#app')
--- src/dev/App.vue
+++ src/dev/App.vue
@@ -1,10 +1,26 @@
 ﻿<template>
-    <div class="container-fluid">
-        <select v-model="name" class="form-control">
-            <option value="DemoServer">Server</option>
-            <option value="DemoClient">Client</option>
-        </select>
-        <component :is="name"></component>
+    <div>
+        <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="/">vue 2.0 bootstrap table</a>
+                </div>
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li><router-link to="/Server">Server</router-link></li>
+                        <li><router-link to="/Client">Client</router-link></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="container body-content">
+            <router-view></router-view>
+        </div>
     </div>
 </template>
 
@@ -12,14 +28,12 @@
     let Vue = require('vue')
 
     module.exports = {
-        data() {
-            return {
-                name: 'DemoClient'
-            }
-        },
-        components: {
-            DemoServer: require('./DemoServer'),
-            DemoClient: require('./DemoClient')
-        }
+
     }
 </script>
+
+<style>
+    body {
+        padding-top: 70px;
+    }
+</style>
