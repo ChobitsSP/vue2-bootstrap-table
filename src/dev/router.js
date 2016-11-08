9ebf798c4e9df8e1599e409066d8474b64c1ef25
Chobits <chobits@live.cn>
2016-11-08 18:00:38 +0800

    https://router.vuejs.org/zh-cn/advanced/lazy-loading.html

--- src/dev/router.js
+++ src/dev/router.js
@@ -3,20 +3,23 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
+const DemoServer = resolve => require(['./DemoServer.vue'], resolve)
+const DemoClient = resolve => require(['./DemoClient.vue'], resolve)
+
 // 2. 定义路由
 // 每个路由应该映射一个组件。 其中"component" 可以是
 // 通过 Vue.extend() 创建的组件构造器，
 // 或者，只是一个组件配置对象。
 // 我们晚点再讨论嵌套路由。
 const routes = [
-  { path: '/Server', component: require('./DemoServer') },
-  { path: '/Client', component: require('./DemoClient') }
+  { path: '/Server', component: DemoServer },
+  { path: '/Client', component: DemoClient }
 ]
 
 // 3. 创建 router 实例，然后传 `routes` 配置
 // 你还可以传别的配置参数, 不过先这么简单着吧。
 const router = new VueRouter({
-    routes // （缩写）相当于 routes: routes
+    routes: routes
 })
 
 module.exports = router
